c78f46fe3aec34d3c54cd6e479c222566e53ced8
Julio Berrocal <36931909+jberrocal2016@users.noreply.github.com>
2021-11-09 22:07:00 -0500

    Add files via upload

---
+++ public_html/public_html/php/connect.php
@@ -0,0 +1,21 @@
+<?php
+	$firstName = $_POST['firstName'];
+	$lastName = $_POST['lastName'];	
+	$email = $_POST['email'];
+	$password = $_POST['password'];	
+
+	// Database connection
+	$conn = new mysqli('localhost','cen4010_fa21_g13','p51sDaSStq','cen4010_fa21_g13');
+	if($conn->connect_error){
+		echo "$conn->connect_error";
+		die("Connection Failed : ". $conn->connect_error);
+	} else {
+		$stmt = $conn->prepare("insert into Registration(firstName, lastName, email, password) values(?, ?, ?, ?)");
+		$stmt->bind_param("ssss", $firstName, $lastName, $email, $password);
+		$execval = $stmt->execute();
+		echo $execval;
+		echo "Registration successfully...";
+		$stmt->close();
+		$conn->close();
+	}
+?>
